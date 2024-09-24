0d2473fcd0f63db39f40b66fd7b0728be018bf68
Tope Taiwo <topzyray@yahoo.com>
2024-09-24 22:00:28 +0100

    Add user account creation feature

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
-      disableErrorMessages: true,
+      // disableErrorMessages: true,
       whitelist: true,
       forbidNonWhitelisted: true,
       transform: true,
---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,53 @@
+import { Transform } from 'class-transformer';
+import {
+  IsDefined,
+  IsEmail,
+  IsEnum,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { UserType } from '../schemas/user.schema';
+
+export class CreateUserDto {
+  @IsEmail()
+  @Transform(({ value }) => value.trim())
+  @IsDefined()
+  email: string;
+
+  @IsString()
+  @Transform(({ value }) => value.trim())
+  @MinLength(8)
+  @Matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+    {
+      message:
+        "Password must countain 1 uppercase, 1 lowercase, any of '@$!%*?&' and 8 characters",
+    },
+  )
+  password: string;
+
+  @IsString()
+  @Transform(({ value }) => value.trim())
+  @MinLength(2)
+  @MaxLength(30)
+  @Matches(/^[A-Za-zÀ-ÿẸỌṢẹọṣ-]+$/, {
+    message: 'Enter valid characters',
+  })
+  firstName: string;
+
+  @IsString()
+  @Transform(({ value }) => value.trim())
+  @MinLength(2)
+  @MaxLength(30)
+  @Matches(/^[A-Za-zÀ-ÿẸỌṢẹọṣ-]+$/, {
+    message: 'Enter valid characters',
+  })
+  lastName: string;
+
+  @IsDefined()
+  @Transform(({ value }) => value.toLowerCase().trim())
+  @IsEnum(UserType)
+  userType: UserType;
+}
